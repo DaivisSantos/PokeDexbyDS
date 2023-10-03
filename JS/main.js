a4f33ca1df6dbdc6e1a2199e4cea937619f915ea
DaivisSa <131676393+DaivisSantos@users.noreply.github.com>
2023-10-03 11:53:23 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,87 @@
+const nomePokemon = document.getElementById("pokemonName");
+const idPokemon = document.getElementById("pokemonNumber");
+const spritesPokemon = document.getElementById("pokemonImage");
+const tipoPokemon = document.getElementById("pokemonType");
+const statusPokemon = document.querySelector('.status');
+const pokemonStatsList = document.getElementById("pokemonStats").querySelectorAll("li");
+
+const typeColors = {
+    electric: 'yellow',
+    fire: 'red',
+    water: 'blue',
+    grass: 'green',
+    ice: 'lightblue',
+    fighting: 'orange',
+    poison: 'purple',
+    ground: 'saddlebrown',
+    flying: 'skyblue',
+    psychic: 'pink',
+    bug: 'limegreen',
+    rock: 'gray',
+    ghost: 'violet',
+    steel: 'silver',
+    dragon: 'royalblue',
+    dark: 'darkslategray',
+    fairy: 'pink'
+};
+
+const fetchpokemon = async (pokemon) => {
+    try {
+        const conexao = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        if (!conexao.ok) {
+            throw new Error(`Erro na requisição: ${conexao.status}`);
+        }
+        const conexaoconvertida = await conexao.json();
+        console.table(conexaoconvertida);
+        return conexaoconvertida;
+    } catch (error) {
+        console.error(`Erro ao buscar Pokémon: ${error.message}`);
+        throw error;
+    }
+}
+
+const mostrarPokemon = async (pokemon) => {
+    try {
+        const data = await fetchpokemon(pokemon);
+
+        nomePokemon.innerHTML = data.name;
+        idPokemon.innerHTML = data.id;
+        spritesPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        const type = data['types'][0]['type']['name'];
+        tipoPokemon.innerHTML = type;
+
+        if (typeColors[type]) {
+            tipoPokemon.style.background = typeColors[type];
+        } else {
+            tipoPokemon.style.background = 'white'; // Cor padrão, caso não esteja na lista de cores.
+        }
+
+        data.stats.forEach((stat, index) => {
+            const NomeStats = stat.stat.name;
+            const StatsNumb = stat.base_stat;
+            
+            pokemonStatsList[index].innerHTML = `${NomeStats}: <span>${StatsNumb}</span>`;
+            pokemonStatsList[index].style.paddingRight = StatsNumb + "%";
+        });
+
+        document.getElementById('error-message').textContent = '';
+    } catch (error) {
+        console.error(`Erro ao mostrar Pokémon: ${error.message}`);
+
+        // Exibe uma mensagem de erro ao usuário
+        document.getElementById('error-message').textContent = 'Pokémon não encontrado. Verifique o nome ou número inserido.';
+    }
+}
+
+const form = document.getElementsByClassName('poke_img');
+const inputSearch = document.getElementById('inputSearch');
+
+inputSearch.addEventListener('keydown', evento => {
+    if(evento.key == 'Enter'){
+        evento.preventDefault();
+        let namePok = evento.target.value;
+        mostrarPokemon(`${namePok}`);
+    }
+});
+
+mostrarPokemon('25');
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# PokeDexbyDS
-Uma criação de uma pokedex como forma de estudo a consumo de API e manipulação do DOM, fazendo a teoria dar luz a praticas.
+# Pokemon.json
+
+This repository provides a basic dataset of all Pokemons.
+
+## Copyright Notice
+
+Please note everything in repository are copyrighted by the Pokémon Company and its affiliates.
+This repository is merely a compilation of data collected by the editors of [Bulbapedia](https://bulbapedia.bulbagarden.net/wiki/Main_Page).
